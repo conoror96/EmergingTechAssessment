a62d74c6aca31011400cc121d9708e2e7a013e9e
Conor O'Reilly <conor@users-MacBook-Pro-245.local>
2019-12-10 18:36:39 +0000

    working on flask app + trying to get predict working on webpage

--- FlaskWebApp/FlaskWebApp.py
+++ FlaskWebApp/FlaskWebApp.py
@@ -1,11 +1,28 @@
-# https://pythonspot.com/flask-hello-world/
-from flask import Flask
+from flask import Flask, render_template,request
+from scipy.misc import imsave, imread, imresize
+import numpy as np
+import keras.models
+import re
+import sys 
+import base64
+import tensorflow as tf
+from keras.models import load_model
+
 app = Flask(__name__)
 
+global model, graph
+
+graph = tf.get_default_graph()
+
+model = load_model('../Notebook/Model/model.h5')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+	#initModel()
+	#render out pre-built HTML file right on the index page
+	return render_template("draw.html")
 
-if __name__ == '__main__':
-    app.run()
+
+if __name__ == "__main__":
+    # run the app locally on the given port
+	app.run(host='0.0.0.0', port=5000, threaded=False)
